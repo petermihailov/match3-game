6f3e347abc26f1f094990913098b03dc3f4dd602
Peter Mihailov <usernameispeter@gmail.com>
2019-05-11 17:09:29 +0300

    update build

---
+++ storybook-static/main.196c72e70be2e426a57f.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p(402),p(487),n.exports=p(871)},487:function(n,o,p){"use strict";p.r(o);p(488),p(713),p(716)}},[[0,1,2]]]);
